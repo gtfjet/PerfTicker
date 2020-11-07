9c0402fdb3cb7c81157e98b26d2e15b5aaa960ad
D. Gelwan <gtfjet@gmail.com>
2020-11-07 18:16:18 -0500

    perf ticket script

---
+++ test.sh
@@ -0,0 +1,12 @@
+
+while :
+do
+  git fetch
+  git pull
+  /c/Python27/python.exe PerfTicker.py
+  git add README.md
+  git commit -m "auto commit"
+  git push
+	echo "Press [CTRL+C] to stop.."
+	sleep 10
+done
