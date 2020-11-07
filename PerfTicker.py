94642445a72fd638f0c97a403549f766e786d1a1
Dave <gtfjet@gmail.com>
2020-11-07 17:51:39 -0500

    Update PerfTicker.py

--- PerfTicker.py
+++ PerfTicker.py
@@ -2,7 +2,7 @@ str = '''
 # Current Status
 | Cost  | Weight | Length |
 |    -  |   -    |  -     |
-| 11.99 |  29    | 1.2    | 
+| 11.99 |  28    | 1.2    | 
 '''
 f = open("README.md", "w")
 f.write(str)
