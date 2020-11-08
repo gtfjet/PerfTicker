632534b07ec9402c675cec27021b60fa4b5d1aff
D. Gelwan <gtfjet@gmail.com>
2020-11-07 21:00:39 -0500

    check if stale

--- PerfTicker.sh
+++ PerfTicker.sh
@@ -1,12 +1,20 @@
+#!/bin/sh
 
 while :
 do
   git fetch
-  git pull
-  /c/Python27/python.exe PerfModel.py
-  git add README.md
-  git commit -m "auto commit"
-  git push
-  echo "Press [CTRL+C] to stop.."
+  LOCAL=$(git rev-parse @)
+  REMOTE=$(git rev-parse @{u})
+  if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+  else
+    echo "Diverged"
+    git pull
+    /c/Python27/python.exe PerfModel.py
+    git add README.md
+    git commit -m "auto commit"
+    git push
+    echo "Press [CTRL+C] to stop.."
+  fi
   sleep 60
 done
