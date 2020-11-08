e0ae0852b722f8032510951a15378e133e3a3dcb
D. Gelwan <gtfjet@gmail.com>
2020-11-07 22:14:03 -0500

    add date and commit id

--- PerfModel.py
+++ PerfModel.py
@@ -1,9 +1,17 @@
-str = '''
+import subprocess
+from datetime import datetime
+now = datetime.now()
+summary = '''
 # Current Status
-| Cost  | Weight | Length |  Height |
-|    -  |   -    |  -     |   -     |
-| 11.99 |  28    | 1.2    |    3.7  |  
+| Date    | Version | Cost   | Weight | Length  |
+|    -    |    -    |    -   |   -    |  -      |
+| YYY     | XXX     |  28    | 1.2    |    3.7  |  
 '''
+ver=subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+print(ver[0:10])
+summary=summary.replace('XXX',ver[0:10]);
+summary=summary.replace('YYY',str(now));
+print(summary)
 f = open("Home.md", "w")
-f.write(str)
+f.write(summary)
 f.close()
