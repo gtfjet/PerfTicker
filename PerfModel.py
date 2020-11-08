f4bdb378467abfa9dbd031d8ec0c873c1b23c38b
Dave <gtfjet@gmail.com>
2020-11-08 15:39:05 -0500

    Update PerfModel.py

--- PerfModel.py
+++ PerfModel.py
@@ -2,7 +2,7 @@ import subprocess
 from datetime import datetime
 now = datetime.now()
 ver = subprocess.check_output(['git', 'rev-parse', 'HEAD'])
-dat = '| YYY | XXX |  29.99  |  1.2  |  2.75  |\n'
+dat = '| YYY | XXX |  29.99  |  1.25  |  2.75  |\n'
 dat = dat.replace('XXX',ver[0:7]);
 dat = dat.replace('YYY',str(now));
 print(dat)
