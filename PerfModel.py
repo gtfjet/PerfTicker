37aa4237ae257d955623858da4bedfc2f84e8c99
Dave <gtfjet@gmail.com>
2020-11-07 20:05:34 -0500

    Update PerfModel.py

--- PerfModel.py
+++ PerfModel.py
@@ -2,7 +2,7 @@ str = '''
 # Current Status
 | Cost  | Weight | Length |  Height |
 |    -  |   -    |  -     |   -     |
-| 11.99 |  28    | 1.2    |    2    |  
+| 11.99 |  28    | 1.2    |    3    |  
 '''
 f = open("README.md", "w")
 f.write(str)
